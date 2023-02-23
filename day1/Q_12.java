69aa05a7ce47d55f75a9a3a238b76c0206a5952c
Hariharan-s005 <h.shakthivelrajagopal@cdw.com>
2023-02-23 13:46:54 +0530

    deleted

--- day1/Q_12.java
+++
@@ -1,8 +0,0 @@
-package day1;
-
-public class Q_12 {
- public static void main(String[] args) {
-	for(int i=0;i<args.length;i++)
-		System.out.println(args[i]+" ");
-}
-}
--- day1/Q_17.java
+++
@@ -1,7 +0,0 @@
-package day1;
-
-public class Q_17 {
-public static void main(String[] args) {
-	
-}
-}
